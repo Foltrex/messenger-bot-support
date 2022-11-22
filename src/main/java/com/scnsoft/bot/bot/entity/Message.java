1fe43a6a6d2bee983c34bf0d8dd885b58554f82e
Foltrex <fpm.chernenkAV@bsu.by>
2022-11-22 16:53:23 +0300

    Add message handler

---
+++ src/main/java/com/scnsoft/bot/bot/entity/Message.java
@@ -0,0 +1,48 @@
+package com.scnsoft.bot.bot.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Message {
+
+    public enum MessageType {
+        hello, //0
+        iam, //1
+        whisper, //2
+        who, //3
+        server,
+        LEAVE_CHAT,
+        ASSIGN_ROLE,
+    }
+
+    private UUID id;
+
+    private UUID sender;
+
+    private UUID receiver;
+
+    private UUID chat;
+
+    private MessageType type;
+
+    private String data;
+
+    private String attachments;
+
+    private String nonce;
+
+    private Instant created;
+}
+
